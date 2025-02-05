342248dbda98bd68053743f6a74dbd9c7ac83c1f
João Vitor <sanvitor2013@gmail.com>
2025-02-05 12:29:42 -0300

    verceu 1.1

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -65,7 +65,7 @@ class UsuarioController {
   static async atualizarUsuario(req, res, next) {
     try {
       const id = req.params.id;
-      await usuario.findByIdAndUpdate(id, req.body);
+      await user.findByIdAndUpdate(id, req.body);
       res.status(200).json({ message: 'Usuario atualiado com sucesso!' });
     } catch (error) {
       console.error(error);
@@ -76,7 +76,7 @@ class UsuarioController {
   static async deletarUsuario(req, res, next) {
     try {
       const id = req.params.id;
-      await usuario.findByIdAndDelete(id);
+      await user.findByIdAndDelete(id);
       res.status(200).json({ message: 'Usuario excluido com Sucesso!' });
     } catch (error) {
       console.error(error);
