e672998e368a04702b1e20e1a752a31400c93bf1
João Vitor <sanvitor2013@gmail.com>
2025-02-06 15:15:45 -0300

    vercel cors

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,11 @@
 import express from 'express';
+import cors from 'cors';
 import usuarios from '../routes/usuarioRouter.js';
 import poster from '../routes/blogRouter.js';
 
 const routes = (app) => {
   app.route('/').get((req, res) => res.status(200).send('APK'));
-
+  app.use(cors());
   app.use(express.json(), usuarios, poster);
 };
 
